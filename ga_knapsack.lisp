726c753a4aa6ea1ad92a1fa9ad14c5d3d3115127
Alex Burkhart <saterus@gmail.com>
2011-05-25 23:43:31 -0400

    Tweaked default values. Added a few more comments.

--- ga_knapsack.lisp
+++ ga_knapsack.lisp
@@ -20,11 +20,15 @@
 ;; Generic Genetic Algorithm Solving
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defparameter *num-generations* 20)
+(defparameter *num-generations* 36)
+(defparameter *mutation-rate* 10)
 
+;; Solve the problem using the Genetic Algorithm
 (defun solve (pop fit)
   (solve-generation pop fit *num-generations* 0))
 
+;; Run the algorithm a maximum number of generations, then return the
+;; current population. Alternatively, we could run until a convergence.
 (defun solve-generation (pop fit max-generation generation-count)
   (let* ((paired (zip (mapcar fit pop) pop))
                  (ranked (sort paired #'> :key #'car )))
@@ -32,23 +36,29 @@
           (t (let ((new-pop (gen-pop ranked '())))
                (solve-generation new-pop fit max-generation (1+ generation-count)))))))
 
+;; Generate the new population out of the new children.
 (defun gen-pop (sorted new-pop)
   (cond ((= (length sorted) (length new-pop))
          new-pop)
         (t (gen-pop sorted (cons (new-child sorted) new-pop)))))
 
+;; Pick two parents, and recombine their DNA to produce a new child.
 (defun new-child (sorted)
   (let* ((a (select-parent sorted))
          (b (select-parent (remove a sorted :test #'(lambda (x y) (eql (cadr y) x))))))
     ;; (format t "Parent A: ~A      Parent B: ~A~%" a b)
     (reproduce a b)))
 
+;; Parent A is a random parent.
+;; Parent B is a "Better" parent than Parent A.
+;; The better the parent is, the more offspring it produces.
 (defun select-parent (sorted)
   (let* ((x (random (length sorted)))
          (y (if (<= (1- x) 0) 0 (random (1- x)))))
     ;; (format t "x: ~A, y: ~A, z: ~A, e: ~A~%" x y (- x y) (elt sorted (- x y)))
     (cadr (elt sorted (- x y)))))
 
+;; Pick a random crossover point for the DNA, then mutate.
 (defun reproduce (x y)
   (let ((crossover (random (length x)))
         (offspring (make-array (length x) :initial-contents x)))
@@ -56,8 +66,7 @@
       (setf (aref offspring i) (aref y i)))
     (mutate offspring)))
 
-(defparameter *mutation-rate* 10)
-
+;; Logical-not a gene with some probability.
 (defun mutate (state)
   (if (< (random 100) *mutation-rate*)
       (let ((index (random (length state))))
@@ -65,7 +74,8 @@
         state)
       state))
 
-(defun gen-random-state (arr-size)
+;; Creates random DNA sequences.
+(defun gen-random-dna (arr-size)
   (let ((arr (make-array arr-size)))
     (dotimes (i arr-size arr)
       (setf (aref arr i) (random-bool)))))
@@ -78,11 +88,13 @@
 (defparameter *ks-pop* NIL)
 (defparameter *ks-size* 500)
 (defparameter *ks-overstuff-penalty* 20)
+(defparameter *ks-pop-size* 200)
 
 ;; Struct representing an item to be placed in the knapsack
 ;; defines make-item, item-p, copy-item, item-size, item-value
 (defstruct item name size value)
 
+;; Reads the possible items from a file.
 (defun read-items (path)
   (with-open-file (str path :direction :input :if-does-not-exist :error)
     (let ((items '()))
@@ -92,8 +104,7 @@
         (push (read-from-string line) items))
       (reverse items))))
 
-;; a Sack is a bit array of items in the sack
-;; list of sacks
+;; a Sack is a bool array indicating which of the items are in the sack
 (defun fill-items (path)
   (let* ((item-list (read-items path))
          (item-array (make-array (length item-list))))
@@ -107,8 +118,10 @@
 (defun ks-generate-population (size)
   (let ((pop '()))
     (dotimes (i size pop)
-      (push (gen-random-state (length *ks-items*)) pop))))
+      (push (gen-random-dna (length *ks-items*)) pop))))
 
+;; Translates a DNA-array into a list of items in the sack.
+;; Useful for computing fitness.
 (defun items-in-sack (bool-arr)
   (let ((item-list '()))
     (dotimes (i (length bool-arr) (reverse item-list))
@@ -147,6 +160,7 @@
                         (mapcar #'item-name soln-sack))))
           (sort solutions #'> :key #'car)))
 
+;; todo: pull this from command line args
 (setf *ks-items* (fill-items (make-pathname :name "camping_items.lisp")))
 
 (defun solve-new (size)
@@ -155,4 +169,4 @@
   (let ((solns (solve *ks-pop* #'ks-fitness)))
     (zip (mapcar #'ks-fitness solns) solns)))
 
-(print-solutions (solve-new 100))
+(print-solutions (solve-new *ks-pop-size*))
